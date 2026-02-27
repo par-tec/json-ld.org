0bae190490dff94220be3c71f9ff10a594b60630
lleonori <lleonori@users.noreply.github.com>
2026-02-27 14:41:16 +0000

    deploy: 1dc35a5c64777efe234d05bd635cc8bf1688d2dc

--- docs/leonori-13-feat-update-the-github-workflow-to-run-in-every-pull-request-not-only-the-ones-pointing-to-main/playground/1.0/playground.js
+++ docs/leonori-13-feat-update-the-github-workflow-to-run-in-every-pull-request-not-only-the-ones-pointing-to-main/playground/1.0/playground.js
@@ -309,7 +309,7 @@
 
     // load the schema
     $.ajax({
-        url: "/schemas/jsonld-schema.json",
+        url: window.BASE_URL + "schemas/jsonld-schema.json",
         dataType: "json"
       })
       .done(function(schema){
